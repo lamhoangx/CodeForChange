2c5045e675c2cd4730b237773c111070b0f9617f
lamhx393 <lamhx393@gmail.com>
2014-08-07 14:13:39 +0700

    Update method post data to server

--- src/hl/codeforchange/fragment/FragmentCapture.java
+++ src/hl/codeforchange/fragment/FragmentCapture.java
@@ -4,8 +4,14 @@ import hl.codeforchange.activity.R;
 import hl.codeforchange.postftp.FTPUploadFile;
 import hl.codeforchange.utils.CacheVariant;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -17,6 +23,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -28,6 +35,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -45,6 +53,10 @@ public class FragmentCapture extends SherlockFragment implements
 	public static ImageView showImg = null;
 	public static Context activity;
 	private Button btnPostData;
+
+	// User
+	private String user = "u488574653";
+	private String pass = "code4change";
 
 	private enum CAPTURE_PICTURE_TYPE {
 		AFTER, BEFORE, QRCODE, NONE
@@ -105,11 +117,70 @@ public class FragmentCapture extends SherlockFragment implements
 	}
 
 	public void postDataToServer() {
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		Log.i("POST TO SERVER ", timeStamp);
 		if (!CacheVariant.PATH_IMG_AFTER.equals("")
 				&& !CacheVariant.PATH_IMG_BEFORE.equals("")
 				&& !CacheVariant.PATH_IMG_QRCODE.equals("")) {
 			FTPUploadFile.uploadFileData(CacheVariant.PATH_IMG_AFTER,
-					"First.jpg", "user", "user");
+					"KFC_After_" + timeStamp + ".jpg", user, pass);
+			FTPUploadFile.uploadFileData(CacheVariant.PATH_IMG_BEFORE,
+					"KFC_Before_" + timeStamp + ".jpg", user, pass);
+			FTPUploadFile.uploadFileData(CacheVariant.PATH_IMG_QRCODE,
+					"KFC_QRCode_" + timeStamp + ".jpg", user, pass);
+
+			// post to mysql
+			try {
+				String username = user;
+				String password = pass;
+				String link = "http://swf.letsgeekaround.com/insert.php";
+				String data = URLEncoder.encode("username", "UTF-8") + "="
+						+ URLEncoder.encode(username, "UTF-8");
+				// data += "&" + URLEncoder.encode("password", "UTF-8")
+				// + "=" + URLEncoder.encode(password, "UTF-8");
+				data += "&" + URLEncoder.encode("billcode", "UTF-8") + "="
+						+ URLEncoder.encode("10010110", "UTF-8");
+				data += "&" + URLEncoder.encode("price", "UTF-8") + "="
+						+ URLEncoder.encode("100000", "UTF-8");
+				data += "&"
+						+ URLEncoder.encode("pic1", "UTF-8")
+						+ "="
+						+ URLEncoder.encode("KFC_After_" + timeStamp + ".jpg",
+								"UTF-8");
+				data += "&"
+						+ URLEncoder.encode("pic2", "UTF-8")
+						+ "="
+						+ URLEncoder.encode("KFC_Before_" + timeStamp + ".jpg",
+								"UTF-8");
+				data += "&"
+						+ URLEncoder.encode("pic3", "UTF-8")
+						+ "="
+						+ URLEncoder.encode("KFC_QRCode_" + timeStamp + ".jpg",
+								"UTF-8");
+				URL url = new URL(link);
+				URLConnection conn = url.openConnection();
+				conn.setDoOutput(true);
+				OutputStreamWriter wr = new OutputStreamWriter(
+						conn.getOutputStream());
+				wr.write(data);
+				wr.flush();
+				// BufferedReader reader = new BufferedReader
+				// (new InputStreamReader(conn.getInputStream()));
+				// StringBuilder sb = new StringBuilder();
+				// String line = null;
+				// // Read Server Response
+				// while((line = reader.readLine()) != null)
+				// {
+				// sb.append(line);
+				// break;
+				// }
+				// String dataRe = sb.toString();
+				// Log.i("Data", dataRe);
+			} catch (Exception e) {
+				Log.i("MySQL ", new String("Exception: " + e.getMessage()));
+			}
+
 		}
 	}
 
@@ -132,7 +203,12 @@ public class FragmentCapture extends SherlockFragment implements
 			captureType = CAPTURE_PICTURE_TYPE.QRCODE;
 			break;
 		case R.id.btnPostToServer:
-			postDataToServer();
+			// postDataToServer();
+			new UpLoadData().execute();
+			// String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+			// .format(new Date());
+			// FTPUploadFile.uploadFileData(CacheVariant.PATH_IMG_AFTER,
+			// "KFC_After"+timeStamp+"jpg", "user", "user");
 			break;
 		}
 
@@ -144,13 +220,13 @@ public class FragmentCapture extends SherlockFragment implements
 		// if (imageUri == null) {
 		// imageUri = getImageFileUri();
 		// }
-		 String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
-		 .format(new Date());
-		 ContentValues values = new ContentValues();
-		 values.put(MediaStore.Images.Media.DATA, timeStamp);
-		 imageUri = context.getContentResolver().insert(
-		 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-//		imageUri = getImageFileUri();
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		ContentValues values = new ContentValues();
+		values.put(MediaStore.Images.Media.TITLE, timeStamp);
+		imageUri = context.getContentResolver().insert(
+				MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+		// imageUri = getImageFileUri();
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
 		startActivityForResult(intent, requestCode);
@@ -228,7 +304,7 @@ public class FragmentCapture extends SherlockFragment implements
 		imagePath = new File(
 				Environment
 						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-			"");
+				"");
 		Log.d(tag, "Find " + imagePath.getAbsolutePath());
 		if (!imagePath.exists()) {
 			if (!imagePath.mkdirs()) {
@@ -254,6 +330,53 @@ public class FragmentCapture extends SherlockFragment implements
 		}
 
 		return Uri.fromFile(image);
+	}
+
+	private class UpLoadDataAfter extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			Toast.makeText(activity, "Upload Finish", Toast.LENGTH_SHORT)
+					.show();
+			super.onPostExecute(result);
+		}
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+		}
+
+	}
+
+	private class UpLoadData extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+
+			postDataToServer();
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			Toast.makeText(activity, "Upload Finish", Toast.LENGTH_SHORT)
+					.show();
+			super.onPostExecute(result);
+		}
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+		}
+
 	}
 
 }
